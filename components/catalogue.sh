ff6bea398d6cffcfc0cf428065068405e7d7cb08
devopsmoon <md_syd01@yahoo.co.in>
2021-06-22 21:41:38 +0530

    added code 4 to catalogue.sh

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -12,14 +12,14 @@ yum install nodejs make gcc-c++ -y
 STAT $?
 HEAD "Add roboshop user app"
 #id roboshop
-#if [ $? -eq 0 ]
-#then
- # echo "User is already exists"
-  #STAT $?
-  #else
-   # useradd -d /home/roboshop -m -s /bin/bash
-    #STAT $?
-    #fi
+if [ $? -eq 0 ]
+then
+  echo "User is already exists"
+  STAT $?
+  else
+    useradd -d /home/roboshop -m -s /bin/bash
+    STAT $?
+    fi
     mkdir -p /home/roboshop/catalogue/
     HEAD "download the source code from github"
     curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/558568c8-174a-4076-af6c-51bf129e93bb/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
@@ -28,4 +28,15 @@ HEAD "Add roboshop user app"
     unzip -o /tmp/catalogue.zip
     STAT $?
     npm install
+    STAT $?
+    HEAD "Modifying service file"
+    sed -i  's/MONGO_ENDPOINT/172.31.10.219/' /home/roboshop/catalogue/systemd.service
+    STAT $?
+    sed -i 's/localhost/172.31.10.219/' /home/roboshop/catalogue/server.js
+    STAT $?
+    HEAD "moving  systemd service file"
+    mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+    STAT $?
+    HEAD "Starting the systemctl services"
+    systemctl start catalogue && systemctl enable catalogue
     STAT $?
