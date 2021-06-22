0391dfad0d9f6e4e0b98ac241a840d0f5ec3ba08
devopsmoon <md_syd01@yahoo.co.in>
2021-06-22 17:39:43 +0530

    added code 4 to mongodb

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -31,7 +31,7 @@ STAT $?
 HEAD "Download the schemas from github"
 curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 cd /tmp
-unzip /tmp/mongodb.zip /tmp
+unzip /tmp/mongodb.zip
 STAT $?
 HEAD "loading the schemas into database"
 mongo < catalogue.js
