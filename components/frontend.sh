1366b39376b2a3435fae357b650b64f72affbc56
devopsmoon <md_syd01@yahoo.co.in>
2021-06-22 15:23:07 +0530

    added code in frontend.sh

--- components/frontend.sh
+++ components/frontend.sh
@@ -6,6 +6,30 @@
 
 source  components/common.sh
 HEAD "Updating system"
-yum updatee -y
+yum update -y
 STAT $?
+yum install epel-release -y
+STAT $?
+yum update -y
+STAT $?
+HEAD "Installing nginx server"
+yum install nginx -y
+STAT $?
+HEAD "Removing the old HTDOCS"
+rm -rf /usr/share/nginx/html/*
+STAT $?
+HEAD "download the source code for github"
+curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+STAT $?
+HEAD "moving to HTDOCS"
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+mv /usr/share/nginx/html/static/* /usr/share/nginx/html/
+mv /usr/share/nginx/localhost.conf /etc/nginx/nginx.conf
+rmdir static
+HEAD "Starting nginx service"
+systemctl start nginx
+systemctl enable nginx
+STAT $?
+
 
