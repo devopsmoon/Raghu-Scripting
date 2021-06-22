8321f3b60954f975ace3f22200b79cf0033ff294
devopsmoon <md_syd01@yahoo.co.in>
2021-06-22 14:54:13 +0530

    changes 3 in common.sh

--- components/common.sh
+++ components/common.sh
@@ -6,10 +6,10 @@ HEAD() {
 STAT() {
   if [ $1 -eq 0 ]
   then
-  echo -e "\e[1;31m done......\e[0m"
+  echo -e "\e[1;31mdone......\e[0m"
   else
-  echo -e "\e[1;31m Fail.....\e[0m"
-  echo -e "\e[1;33m check the logs for more details..... logfile: /tmp/logs\e[0m"
+  echo -e "\e[1;31mFail.....\e[0m"
+  echo -e "\e[1;33mcheck the logs for more details..... logfile: /tmp/logs\e[0m"
   exit 1
   fi
 }
