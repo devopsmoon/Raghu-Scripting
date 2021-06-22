742d6875b3763ec2e6658f0db323141db960c414
devopsmoon <md_syd01@yahoo.co.in>
2021-06-22 14:52:52 +0530

    changesq in common.sh

--- components/common.sh
+++ components/common.sh
@@ -9,7 +9,7 @@ STAT() {
   echo -e "\e[1;31m done......\e[0m"
   else
   echo -e "\e[1;31m Fail.....\e[0m"
-  echo -e "\e[1;3m check the logs for more details..... logfile: /tmp/logs\e[0m"
+  echo -e "\e[1;33m check the logs for more details..... logfile: /tmp/logs\e[0m"
   exit 1
   fi
 }
